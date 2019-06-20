19adeaf3bb13805bb2a03e8c8aafdd4bf8d077ff
MiguelCPJava <miguelcpjava@gmail.com>
2019-06-20 12:27:35 -0300

    Foi adicionados mais imagens tanto de treinamento como de validação, adicionado mais uma camada densa, e o modelo está co uma acuracia de 90%, porém há o problema de reconheciemnto de todas camadas por todas camadas, ou seja, o que deveria só reconhecer a primeira camada, e falo camada o número que representa a primeira camada, está reconhecendo todas as camadas.

--- mestrado.py
+++ mestrado.py
@@ -1,6 +1,6 @@
 import numpy as np
 import keras
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from keras import backend as K
 from keras import metrics
 from keras.models import Sequential
@@ -13,52 +13,66 @@ from keras.metrics import categorical_crossentropy
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import *
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img, image
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+import os
+import glob
+from PIL import Image
+
+def get_immediate_subdirectories(data_directory):
+    return [name for name in os.listdir(data_directory)
+            if os.path.isdir(os.path.join(data_directory, name))]
+
 
 train_data_dir = 'data/train' 
-validation_data_dir = 'data/validation' 
-test_data_dir = 'data/test'
+validation_data_dir = 'data/validation'
 nb_train_samples = 1000
 nb_validation_samples = 36
-epochs = 20
-batch_size = 5 #Número de classes
+epochs = 10
+batch_size = 32  #Numero de classes
 # dimensions of our images of validation.
 img_width, img_height = 50, 50
 
-adam = Adam(lr=0.0005)#Adam(lr=0.001)
+adam = Adam(lr=0.001)#Adam(lr=0.001)
 rmsprop = RMSprop() #RMSprop(lr=0.0005)
 
-#Iniciando a construção da rede convolucional
+#Iniciando a construcao da rede convolucional
 #Criando o modelo
 classifier = Sequential()
 # 1st Layer
-classifier.add(Conv2D(64, (3, 3), input_shape = (img_width, img_height, 1), activation = 'relu'))
+classifier.add(Conv2D(40, (5, 5), input_shape = (img_width, img_height, 1), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2), strides=(1,1)))
 #classifier.add(BatchNormalization())
 #classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 #classifier.add(MaxPooling2D(pool_size = (2, 2)))
-#O dropout consiste em configurar aleatoriamente uma taxa de fração de unidades de entrada para 0 
-#em cada atualização durante o tempo de treinamento, o que ajuda a prevenir o overfitting.
-classifier.add(Dropout(0.15))
+#O dropout consiste em configurar aleatoriamente uma taxa de fracao de unidades de entrada para 0
+#em cada atualizacao durante o tempo de treinamento, o que ajuda a prevenir o overfitting.
+classifier.add(Dropout(0.2))
 classifier.add(Flatten())
 #Adicionando as camadas
 #1 fully connected layers
 classifier.add(Dense(units = 80, activation = 'relu'))
-classifier.add(Dropout(0.25))
+classifier.add(Dropout(0.2))
 #2 fully connected layers
-classifier.add(Dense(units = 40, activation = 'relu'))
-classifier.add(Dropout(0.25))
+classifier.add(Dense(units = 100, activation = 'relu'))
+classifier.add(Dropout(0.2))
 #3 fully connected layers
+classifier.add(Dense(units = 150, activation = 'relu'))
+classifier.add(Dropout(0.2))
+#4 fully connected layers
 classifier.add(Dense(units = 5, activation = 'softmax'))
 classifier.compile(optimizer = rmsprop, loss = 'categorical_crossentropy', metrics = ['accuracy', metrics.categorical_accuracy])
 
 #Gerando a base de treinamento
 train_datagen = ImageDataGenerator(rescale = 1./255,
-									rotation_range=30,
-									zoom_range=0.20,
-									brightness_range=[0.6, 1.4])
+                                   width_shift_range=0.02,
+                                   height_shift_range=0.02,
+                                    rotation_range=10,
+                                    zoom_range=0.10,
+                                    brightness_range=[0.7, 1.3])
 #width_shift_range=0.2,
 #height_shift_range=0.2
-#Gerando a base de validação
+#Gerando a base de validacao
 validation_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory(train_data_dir,
@@ -69,39 +83,78 @@ training_set = train_datagen.flow_from_directory(train_data_dir,
                                                  class_mode = 'categorical')
 
 validation_set = validation_datagen.flow_from_directory(validation_data_dir,
-														target_size = (img_width,img_height),
-														shuffle=False,
-														color_mode  = 'grayscale',
-														batch_size  = 1,
-														class_mode  = 'categorical')
-
+                                                        target_size = (img_width,img_height),
+                                                        shuffle=False,
+                                                        color_mode  = 'grayscale',
+                                                        batch_size  = 1,
+                                                        class_mode  = 'categorical')
+validation_set.reset()
 print("Indices: "+str(training_set.class_indices))
-#Número do conjunto de treinamento
+#Numero do conjunto de treinamento
 print("====>"+str(training_set.n))
 
 history = classifier.fit_generator(training_set,
-                         steps_per_epoch = (training_set.n//batch_size) * 5,
+                         steps_per_epoch = (training_set.samples//batch_size) * 30,
                          epochs = epochs,
                          validation_data=validation_set,
-                         validation_steps = nb_validation_samples,
+                         validation_steps = validation_set.samples,
                          verbose=1)
 
 classifier.summary()
 #score_validation = classifier.evaluate_generator(validation_set, 140)
-score_validation = classifier.evaluate_generator(generator=validation_set,steps=epochs,verbose=1)
+score_validation = classifier.evaluate_generator(generator=validation_set,steps=validation_set.n,verbose=1)
 score = classifier.predict_generator(validation_set, 140)
 
 #print(str(score))
-print("Accuracy = ","%.2f" % (score_validation[1]*100),"%")
-print("Loss = ","%.2f" % (score_validation[0]*100),"%")
+#print("Accuracy = ","%.2f" % (score_validation[1]*100),"%")
+#print("Loss = ","%.2f" % (score_validation[0]*100),"%")
+
+
+#inicializacao
+mylist = []
+mylistY = []
+
+#pegando subpastas com cada classe
+dir_list = get_immediate_subdirectories(validation_data_dir)
+dir_list.sort()
+
+curr_class=-1
+target_names = ['', '', '', '', '']
+
+for data_dir in dir_list: #para cada pasta de classe
+    curr_class=curr_class+1
+    target_names[curr_class]=data_dir
+    
+    #pega as imagens da pasta
+    image_list = [os.path.basename(x) for x in glob.glob(os.path.join(validation_data_dir, data_dir) + '/*.jpg')]
+    for img in image_list: #para cada imagem da passa da classe
+        img_pil_1 = Image.open(validation_data_dir + '/' + data_dir + '/' + img).convert('L') #convertendo para greyscale
+        img_pil_150 = img_pil_1.resize((img_width,img_height)) #resize
+        img_np_150 = (np.asarray(img_pil_150).astype('float32'))/255 #transformando em array de floats de [0,1]
+        img_np_150_rsh = img_np_150.reshape(1,img_width,img_height,1) #reshape
+        pred=classifier.predict(img_np_150_rsh)[0] #predict
+        mylist.append(pred) #coloca o predict na lista de predicoes
+        mylistY.append(curr_class) #coloca o valor verdadeiro na lista de valores verdadeiros
+
+y_pred = np.argmax(mylist, axis=1)
+
+print('Accuracy')
+print(accuracy_score(mylistY,y_pred))
+
+print('Confusion Matrix')
+print(confusion_matrix(mylistY,y_pred))
+
+#exit()
+
 
 model_json = classifier.to_json()
-with open("model.json", "w") as json_file:
+with open("Model/model.json", "w") as json_file:
     json_file.write(model_json)
     #Carregar o json e classificar com a imagem total
-classifier.save_weights("model_weights.h5")
-classifier.save("model.h5")
+classifier.save_weights("Model/model_weights.h5")
+classifier.save("Model/model.h5")
 
+exit()
 
 acc = history.history['acc']
 val_acc = history.history['val_categorical_accuracy']
@@ -110,16 +163,16 @@ val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
 #plt.style.use("ggplot")
-plt.plot(epochs, acc, 'bo', label='Training acc',color="blue")
-plt.plot(epochs, val_acc, 'b', label='Validation acc',color="green")
-plt.title('Training and validation accuracy')
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+#plt.plot(epochs, acc, 'bo', label='Training acc',color="blue")
+#plt.plot(epochs, val_acc, 'b', label='Validation acc',color="green")
+#plt.title('Training and validation accuracy')
+#plt.legend()
+#plt.figure()
+#plt.plot(epochs, loss, 'bo', label='Training loss')
+#plt.plot(epochs, val_loss, 'b', label='Validation loss')
+#plt.title('Training and validation loss')
+#plt.legend()
+#plt.show()
 
 #Proximo passo, pegar a imagem original, pecorrer em dois for, para cortar e pintar o centro, 
 #Ao pecorrer na escala de 50x50 para achar o positivo e negativo
--- predicao.py
+++ predicao.py
@@ -14,32 +14,33 @@ import numpy as np
 from PIL import Image as pli
 from PIL import ImageChops
 import os
+import time
 
-#Método alternativo em numpy para carregar a iamgem e transformar em grayscale
+#Metodo alternativo em numpy para carregar a iamgem e transformar em grayscale
 def load(filename):
-   np_image = filename.convert('L')
-   np_image = np.array(filename)
-   np_image = np.array(np_image).astype('float32')/255
-   np_image = transform.resize(np_image, (50,50, 1))
-   np_image = np.expand_dims(np_image, axis=0)
- #  np_image = preprocess_input(np_image)
-   return np_image
+    np_image = filename.convert('L')
+    np_image = np.array(filename)
+    np_image = np.array(np_image).astype('float32')/255
+    np_image = transform.resize(np_image, (50,50, 1))
+    np_image = np.expand_dims(np_image, axis=0)
+    #  np_image = preprocess_input(np_image)
+    return np_image
 
-#Método para carregar imagem a partir da biblioteca opencv
-#junto com o código de pecorrer pixel a pixel
+#Metodo para carregar imagem a partir da biblioteca opencv
+#junto com o codigo de pecorrer pixel a pixel
 def load_image_cv(path):
-	img_read = cv2.imread(path)
-	img_read = cv2.cvtColor(img_read, cv2.COLOR_BGR2GRAY)
-	largura, altura = img_read.shape
-	file_name_save = save_path+"/imgpixel"
-	print("Informações: "+str(img_read.shape))
-	print("transformando em grayscale...")
-	for x in range(i,largura):
-		for y in range(j,altura):
-			print("Pixel: "+str(x)+" - "+str(y))
-			nova_imagem = img_read[x:x+altura, y:y+largura]
-			if y == 1000:
-				cv2.imwrite(file_name_save+"{1}-{0}".format(y,x)+".jpeg",nova_imagem)
+    img_read = cv2.imread(path)
+    img_read = cv2.cvtColor(img_read, cv2.COLOR_BGR2GRAY)
+    largura, altura = img_read.shape
+    file_name_save = save_path+"/imgpixel"
+    print("Informacoes: "+str(img_read.shape))
+    print("transformando em grayscale...")
+    for x in range(i,largura):
+        for y in range(j,altura):
+            print("Pixel: "+str(x)+" - "+str(y))
+            nova_imagem = img_read[x:x+altura, y:y+largura]
+            if y == 1000:
+                cv2.imwrite(file_name_save+"{1}-{0}".format(y,x)+".jpeg",nova_imagem)
 
 
 
@@ -52,20 +53,22 @@ path_h5_weight = 'model/model_weights.h5'
 loaded_model = load_model(path_h5)
 loaded_model.load_weights(path_h5_weight)
 
-#Marcações
-# As cores são defindas para cada classe, verde para classe L1, amarela L2, laranja L3 evermelha classe L4
+#Marcacoes
+# As cores sao defindas para cada classe, verde para classe L1, amarela L2, laranja L3 evermelha classe L4
 verde = (0,255,0)
 amarelo = (53,225,255)
 laranja = (0,128,255)
 vermelho =  (0, 0, 255)
+azul =  (255, 0, 5)
 
-#As imagens que foram utilizadas no treinamento e validação foram de 50x50
+
+#As imagens que foram utilizadas no treinamento e validacao foram de 50x50
 pattern_img_size = 50,50
 #https://towardsdatascience.com/from-raw-images-to-real-time-predictions-with-deep-learning-ddbbda1be0e4
-#Caminho onde estão e para onde vão os arquivos para experimento
+#Caminho onde estao e para onde vao os arquivos para experimento
 base_path = "data/test/"
 save_path = "data/test/save5/"
-path_img = "data/test/casa120.jpg"
+path_img = "data/test/Casa141.jpg"
 image_process = None
 #Metodo para reduzir a imagem 
 def trim(im):
@@ -76,122 +79,120 @@ def trim(im):
     if bbox:
         return im.crop(bbox)
 
-## a ideia é pegar a imagem original e pecorrer pixel a pixel (50x50), fazer o crop e a partir desta imagem
-#  fazer o predict para saber se é uma das classes ou não
+## a ideia e pegar a imagem original e pecorrer pixel a pixel (50x50), fazer o crop e a partir desta imagem
+#  fazer o predict para saber se e uma das classes ou nao
 ## Quando for, tem que ser marcado com uma cor mais clara e passar para o proximo
-# no final é retornado a imagem com a marcação.
+# no final e retornado a imagem com a marcacao.
 #def threshold_slow(T, image):
 def threshold_slow(image):
-	camada1 = 1;
-	camada2 = 1;
-	camada3 = 1;
-	camada4 = 1;
-	#Transformar a imagem em array de bytes
-	image_process = np.array(image)
-	cv2.imwrite(save_path+"regua.jpg",image_process)
-	#Pasta onde fica alojados os arquivos cortados
-	path_crop_save = save_path+"/crop/"
-	print("Carregando imagem....")
-	#Nome padrão para os arquivos
-	file_name_save = "/imgpixel"
-	text = ""
-	#Obtendo a dimenssão da imagem escolhida
-	largura, altura = image.size
-	#Fazendo o calculo da proporção
-	proporcao = largura/altura
-	print("Largura: "+str(largura))
-	print("Altura: "+str(altura))
-	print("Proporção: "+str(proporcao))
-	#Obtendo o pixel da imagem
-	pixels = list(image.getdata())
-	#print("Pixels: "+str(pixels))
-	#Loop para percorrer a imagem
-	for y in range(0, altura-50):
-		for x in range(0, largura-50):
-			#Inicialmente é realizado o corte na dimensão de 50x50
-			#Crop (Esquerda, cima, direita, baixo)
-			new_image = image.crop((x,y,x+50,y+50)).copy()
-			#A imagem cortada é transformada em array pois o método de predicação só aceita iamgem em array
-			crop_img_array = np.array(new_image)
-			#Essa nova imagem cortada está em 3 canais(RGB), com isso é acionado o método load 
-			#para transformar em array e em um canal (graysacle)
-			img_gray = load(new_image)
-			#A ideia é marcar o pixel central, na imagem orignal,da que foi cortada para verificar a execução
-			#com isso há o método threshold no opencv para binarizar a iamgem e posteriormente
-			#calcular o momento da binarização da imagem, que isto é realizado pelo método moments
-			ret,thresh = cv2.threshold(img_gray,127,255,0)
-			M = cv2.moments(thresh)
-			image_class = loaded_model.predict(img_gray,verbose=0)
-			#print(">>>>>>>"+str(image_class))
-			#Atribui o valor da classe classificada
-			pred = image_class.argmax(axis=-1)
-			''' Lembrando que o 0 é o numero 7 na régua, bem como
-				o 1 é o 8, 2 é o 13 e o 3 é 14, já para finalizar
-				4 são as negativas ou lixo
-			'''
-			if pred == 0:
-				#Cada corte é salvo na pasta declarada abaixo para comparar se realmente está fazendo correto
-				crop_write = ""
-				crop_write = path_crop_save+"L1/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
-				cv2.imwrite(crop_write,crop_img_array)
-				print("Cortou na L1 pela "+str(camada1)+"ª vez")
-				camada1 = camada1+1
-				#Aqui é localizado o centro da imagem, ou seja, do 50x50 e depois marca com a cor de cada camada
-				#declarada, neste caso a verde são para identificar o número 7 na régua.
-				if M["m00"] != 0:
-					cX = int(M["m10"] / M["m00"])
-					cY = int(M["m01"] / M["m00"])
-				else:
-					#cX, cY = 0, 0
-					cX, cY = x, y
-				loda_img = cv2.imread(save_path+"regua.jpg")
-				loda_img = cv2.circle(loda_img, (cX, cY), 3, verde, -1)
-				cv2.imwrite(save_path+"regua.jpg",loda_img)
-			elif pred == 1:
-				crop_write = ""
-				crop_write = path_crop_save+"L2/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
-				cv2.imwrite(crop_write,crop_img_array)
-				print("Cortou na L2 pela "+str(camada2)+"ª vez")
-				camada2 = camada2+1
-				if M["m00"] != 0:
-					cX = int(M["m10"] / M["m00"])
-					cY = int(M["m01"] / M["m00"])
-				else:
-					cX, cY = x, y
-				loda_img = cv2.imread(save_path+"regua.jpg")	
-				loda_img = cv2.circle(loda_img, (cX, cY), 3, amarelo, -1)
-				cv2.imwrite(save_path+"regua.jpg",loda_img)
-			elif pred == 2:
-				crop_write = ""
-				crop_write = path_crop_save+"L3/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
-				cv2.imwrite(crop_write,crop_img_array)
-				print("Cortou na L3 pela "+str(camada3)+"ª vez")
-				camada3 = camada3+1
-				if M["m00"] != 0:
-					cX = int(M["m10"] / M["m00"])
-					cY = int(M["m01"] / M["m00"])
-				else:
-					#cX, cY = 0, 0
-					cX, cY = x, y
-				loda_img = cv2.imread(save_path+"regua.jpg")	
-				loda_img = cv2.circle(loda_img, (cX, cY), 3, laranja, -1)
-				cv2.imwrite(save_path+"regua.jpg",loda_img)
-			elif pred == 3:
-				crop_write = ""
-				crop_write = path_crop_save+"L4/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg" 
-				cv2.imwrite(crop_write,crop_img_array)
-				print("Cortou na L4 pela "+str(camada4)+"ª vez")
-				camada4 = camada4+1
-				if M["m00"] != 0:
-					cX = int(M["m10"] / M["m00"])
-					cY = int(M["m01"] / M["m00"])
-				else:
-					#cX, cY = 0, 0
-					cX, cY = x, y
-				loda_img = cv2.imread(save_path+"regua.jpg")	
-				loda_img = cv2.circle(loda_img, (cX, cY), 3, vermelho, -1)
-				cv2.imwrite(save_path+"regua.jpg",loda_img)
-			else: result = "nenhum"
+    #Transformar a imagem em array de bytes
+    image_process = np.array(image)
+    #Pasta onde fica alojados os arquivos cortados
+    path_crop_save = save_path+"/crop/"
+    print("Carregando imagem....")
+    #Nome padrao para os arquivos
+    file_name_save = "/imgpixel"
+    text = ""
+    #Obtendo a dimenssao da imagem escolhida
+    largura, altura = image.size
+    #Fazendo o calculo da proporcao
+    proporcao = largura/altura
+    print("Largura: "+str(largura))
+    print("Altura: "+str(altura))
+    print("Proporcao: "+str(proporcao))
+    #Obtendo o pixel da imagem
+    pixels = list(image.getdata())
+    numl1=numl2=numl3=numl4=numl5=0
+    #print("Pixels: "+str(pixels))
+    #Loop para percorrer a imagem
+    for y in range(0, altura-50):
+        print('linha',y)
+        for x in range(0, largura-50):
+            #Inicialmente e realizado o corte na dimensao de 50x50
+            #Crop (Esquerda, cima, direita, baixo)
+            new_image = image.crop((x,y,x+50,y+50)).copy()
+            #A imagem cortada e transformada em array pois o metodo de predicacao so aceita iamgem em array
+            crop_img_array = np.array(new_image)
+                #Essa nova imagem cortada esta em 3 canais(RGB), com isso e acionado o metodo load
+                #para transformar em array e em um canal (graysacle)
+                
+            #img_gray = load(new_image)
+            img_gray = new_image.convert('L')
+            #     img_gray.show()
+            time.sleep(0.5)
+            img_gray = (np.asarray(img_gray).reshape(1,50,50,1))/255.0
+                #A ideia e marcar o pixel central, na imagem orignal,da que foi cortada para verificar a execucao
+                #com isso ha o metodo threshold no opencv para binarizar a iamgem e posteriormente
+                #calcular o momento da binarizacao da imagem, que isto e realizado pelo metodo moments
+                # ret,thresh = cv2.threshold(img_gray,127,255,0)
+                # M = cv2.moments(thresh)
+            image_class = loaded_model.predict(img_gray,verbose=0)
+            print(">>>>>>>"+str(image_class))
+            #Atribui o valor da classe classificada
+            # print(img_gray)
+            #exit()
+            pred = image_class.argmax(axis=-1)
+            ''' Lembrando que o 0 e o numero 7 na regua, bem como
+                        o 1 e o 8, 2 e o 13 e o 3 e 14, ja para finalizar
+                        4 sao as negativas ou lixo
+                        '''
+            
+            if pred == 0:
+                numl1=numl1+1
+                #   time.sleep(0.5)
+                        #Cada corte e salvo na pasta declarada abaixo para comparar se realmente esta fazendo correto
+                crop_write = ""
+                crop_write = path_crop_save+"L1/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
+                cv2.imwrite(crop_write,crop_img_array)
+                            #Aqui e localizado o centro da imagem, ou seja, do 50x50 e depois marca com a cor de cada camada
+                            #declarada, neste caso a verde sao para identificar o numero 7 na regua.
+                cX=x+25
+                cY=y+25
+                image_process = cv2.circle(image_process, (cX, cY), 5, verde, -1)
+                cv2.imwrite(save_path+"regua.jpg",image_process)
+            elif pred == 1:
+                numl2=numl2+1
+                #     time.sleep(0.5)
+                crop_write = ""
+                crop_write = path_crop_save+"L2/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
+                cv2.imwrite(crop_write,crop_img_array)
+                cX=x+25
+                cY=y+25
+                image_process = cv2.circle(image_process, (cX, cY), 5, amarelo, -1)
+                cv2.imwrite(save_path+"regua.jpg",image_process)
+            elif pred == 2:
+                numl3=numl3+1
+                #         time.sleep(0.5)
+                crop_write = ""
+                crop_write = path_crop_save+"L3/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
+                cv2.imwrite(crop_write,crop_img_array)
+                cX=x+25
+                cY=y+25
+                image_process = cv2.circle(image_process, (cX, cY), 5, laranja, -1)
+                cv2.imwrite(save_path+"regua.jpg",image_process)
+            elif pred == 3:
+                numl4=numl4+1
+                #          time.sleep(0.5)
+                crop_write = ""
+                crop_write = path_crop_save+"L4/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
+                cv2.imwrite(crop_write,crop_img_array)
+                cX=x+25
+                cY=y+25
+                image_process = cv2.circle(image_process, (cX, cY), 5, vermelho, -1)
+                cv2.imwrite(save_path+"regua.jpg",image_process)
+            else:
+                #print("Nernhum")
+                numl5=numl5+1
+                print(str(numl5)+"ª vez")
+                #crop_write = ""
+                #crop_write = path_crop_save+"L5/"+file_name_save+"{0}-{1}".format(x,y)+".jpeg"
+                #cv2.imwrite(crop_write,crop_img_array)
+                #cX=x+25
+                #cY=y+25
+                #image_process = cv2.circle(image_process, (cX, cY), 5, azul, -1)
+                #cv2.imwrite(save_path+"regua.jpg",image_process)
+                    
+    print(numl1,numl2,numl3,numl4,numl5)
 
 img = pli.open(path_img)
 threshold_slow(img)
